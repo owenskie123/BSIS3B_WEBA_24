a920c994a0082e4cca962da87a8fbd2764332983
allenromarate@gmail.com <kimmark0922@gmail.com>
2024-02-23 07:59:21 +0800

    added

---
+++ mvc/app/core/init.php
@@ -0,0 +1,10 @@
+<?php
+
+require 'config.php';
+require 'functions.php';
+require 'Database.php';
+require 'Model.php';
+require 'Controller.php';
+require 'App.php';
+
+
--- mvc/public/index.php
+++ mvc/public/index.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <center>
-    <h1>I LOVE YOU VIRUS </h1>
-</center>
-</body>
-</html>
+ <?php
+
+ require '../app/core/init.php'; 
+ $app = new App();
