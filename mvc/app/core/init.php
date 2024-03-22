a9fb37b57ef028bccb7e017a29ed6817aeabf373
jaypeepualengco6 <157766987+jaypeepualengco6@users.noreply.github.com>
2024-03-22 09:21:39 +0800

    Add files via upload

--- mvc/app/core/init.php
+++ mvc/app/core/init.php
@@ -1,8 +1,12 @@
 <?php
 
 require 'functions.php';
-require 'App.php';
+require 'Database.php';
 require 'Controller.php';
+require 'Model.php';
+require 'App.php';
 
-
-
+spl_autoload_register(function ($class_name)
+{
+    require '../app/models/' . $class_name . '.php';
+});
