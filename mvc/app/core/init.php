85b8201a3d4e94431a00f62340913727b38a5a64
Ehsige <danisemores@gmail.com>
2024-03-22 08:55:39 -0700

    added

--- mvc/app/core/init.php
+++ mvc/app/core/init.php
@@ -3,6 +3,11 @@
 require 'functions.php';
 require 'App.php';
 require 'Controller.php';
+require 'Database.php';
+require 'Model.php';
 
-
-
+spl_autoload_register(function ($class_name)
+{
+    require '../app/models/' .$class_name . '.php';
+}
+)
