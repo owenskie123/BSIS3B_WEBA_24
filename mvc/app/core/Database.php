648123f7f2b6a95b08d6ded55bd2b0380281d514
Ehsige <danisemores@gmail.com>
2024-03-22 08:14:09 -0700

    added

--- mvc/app/core/Database.php
+++ mvc/app/core/Database.php
@@ -10,5 +10,22 @@ class  Database
         return $con;
     }
 
+    public function query($query, $data = [])
+    {
+        $con = $this->connect();
+        $stm = $con->prepare($query);
+
+        $check = $stm->execute($data);
+
+        if ($check) {
+            $result= $stm->fetchAll(PDO::FETCH_OBJ);
+
+            if(is_array($result) && count($result) >0){
+                return $result;
+            }
+        }
+        return false;
+    }
+
 
 }
